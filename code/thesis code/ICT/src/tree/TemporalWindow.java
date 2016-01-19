6e2fda3b65bf0b548e3f656d780d57e800802bff
donato mastropasqua <mastropasquadonato@gmail.com>
2016-01-19 23:16:50 +0100

    upload class TemporalWindow

---
+++ code/thesis code/ICT/src/tree/TemporalWindow.java
@@ -0,0 +1,83 @@
+package tree;
+
+
+
+import org.apache.commons.collections4.queue.CircularFifoQueue;
+
+import data.feature.Feature;
+
+public class TemporalWindow<Object> extends CircularFifoQueue<Object> implements Cloneable{
+
+	/*
+	 * dimensione massima della 
+	 * finestra temporale
+	 */
+	private static int windowMaxSize;
+	
+	private String featureName;
+	
+	/*
+	 * costruttore della classe
+	 * istanzia la coda in base alla dimensione massima
+	 * e memorizza il nome della feature a cui fa riferimento 
+	 * la finestra temporale
+	 */
+	TemporalWindow(Feature f){
+		super(windowMaxSize);
+		this.featureName=f.getName();
+	}
+	
+	/*
+	 * metodo di classe
+	 * setta l'attributo window max size
+	 */
+	public static void setWindowMaxSize(int dim){
+		windowMaxSize=dim;
+	}
+	
+	
+	public int getwindowMaxSize(){
+		return windowMaxSize;
+	}
+	
+	/*
+	 * aggiorna la finestra temporale 
+	 * aggiungendo ad essa la nuova media
+	 */
+	void updateTemporalWindow(Feature f) {
+		this.add((Object) f.getPrototype());
+	}
+	
+	/*
+	 * aggiorna la finestra temporale
+	 * aggiungendo ad essa null
+	 */
+	void insNull(){
+		this.add((Object) "null");
+	}
+	
+	/*
+	 * rimuovere bug null
+	 */
+	public String toString(){
+		String s=this.featureName+ ": ";
+		
+		for (Object o: this){
+			s=s+o + " ";
+		}
+		
+		return s;
+	}
+	
+	
+	public TemporalWindow clone() throws CloneNotSupportedException{
+		
+		TemporalWindow t= (TemporalWindow)super.clone();
+		for(Object o: this){
+			t.add(o);
+		};
+		return t;
+	}
+	
+	
+}
