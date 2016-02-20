f3899fa7243ef062c0690b865925d8af5a629d03
Corrado Giancaspro <c.giancaspro5@gmail.com>
2016-02-20 11:13:13 +0100

    Utils OK

--- code/thesis code/ICT/src/varUtility/OptimarVARModel.java
+++ code/thesis code/ICT/src/varUtility/OptimarVARModel.java
@@ -14,13 +14,22 @@ public class OptimarVARModel {
 	public ArrayList<FeatureForecastingModel> computOptimalVARModel(
 			HashMap<String, ForecastingModel> hm, SnapshotSchema schema) {
 		ArrayList<FeatureForecastingModel> optModel = new ArrayList<FeatureForecastingModel>();
-
+		double min;
+		FeatureVARForecastingModel FeatureDefModel = null;
 		for (Feature f : schema.getTargetList()) {
-
+			min = Double.MAX_VALUE;
+			FeatureDefModel = null;
 			for (String comb : hm.keySet()) {
 				ForecastingModel model = hm.get(comb);
-
+				FeatureVARForecastingModel featureModel = (FeatureVARForecastingModel) model
+						.getFeatureForecastingModel(f);
+				if (featureModel.getRMSE() < min) {
+					min = featureModel.getRMSE();
+					FeatureDefModel = featureModel;
+				}
 			}
+			optModel.add(FeatureDefModel.getFeature().getFeatureIndex(),
+					FeatureDefModel);
 		}
 		return optModel;
 	}
