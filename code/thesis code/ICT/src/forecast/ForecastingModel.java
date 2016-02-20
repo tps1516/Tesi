3e17a07df3fcb0695443aff82a4cde2d320b8037
donato mastropasqua <mastropasquadonato@gmail.com>
2016-02-20 21:14:22 +0100

    classe forecasting model completata

--- code/thesis code/ICT/src/forecast/ForecastingModel.java
+++ code/thesis code/ICT/src/forecast/ForecastingModel.java
@@ -13,23 +13,114 @@ import java.util.Iterator;
 
 import data.feature.Feature;
 
+/*
+ * tale classe ha la responsabilità di modellare il modello VAR
+ * per ciascuna FEATURE dello schema
+ */
 public class ForecastingModel implements Iterable<FeatureForecastingModel>,
 		Serializable {
 
+	/*
+	 * ha la responsabilità di memorizzare i modelli VAR di ciascuna FEATURE
+	 * la posizione in cui il modello VAR viene memorizzato nell'arrayList
+	 * corrisponde all'indice della feature stessa nello schema
+	 */
 	private ArrayList<FeatureForecastingModel> models;
 
+	/*
+	 * costruttore della classe
+	 * ha la responsibilità di istanziare uno oggetto 
+	 * tale che il modello VAR corrispondete alla FEATURE sia il migliore
+	 * rispetto al RMSE ottenuto variando le possibili combinazini sui parametri 
+	 * di input del modello VAR (type e ic)
+	 */
 	public ForecastingModel(double[][] dataset, SnapshotSchema schema,
 			ArrayList<Object> rParameters) {
 		HashMap<String, ArrayList<Object>> resultR;
 		HashMap<String, ForecastingModel> resultRToJava;
 		RForecast r = new RVar();
+		
+		/*
+		 * richiamo il metodo RForecasting e memorizzo 
+		 * i risultati ottenuti dalla chiamata nell'oggetto resultR
+		 * tale oggetto è un hashmap tale che l'insieme delle chiavi
+		 * rappresenta l'insieme di tutte le combinazioni possibili dei parametri di input
+		 * al modello VAR mentre l elemento (arrayList<Object>) associato alla specifica
+		 * chaive è il risultato ottenuto per tale configurazione
+		 */
 		resultR = r.RForecasting(dataset, schema, rParameters);
 		FromRToJava fRToJava = new FromRToJava();
+		
+		/*
+		 * converto la struttura dati ottenuta dalla chiamata precedente
+		 * in una struttura analoga che però memorizza per ciascuna chiave
+		 * un oggetto ForecastingModel 
+		 * ogni oggetto corrisponde ad una specifica combinazione di parametri
+		 */
 		resultRToJava = fRToJava.converter(resultR, schema,
 				doubleToDouble(dataset));
 		OptimalVARModel opt = new OptimalVARModel();
+		
+		/*
+		 * costruisco l'oggetto forecastingmodel finale 
+		 * dove il modello var per ciascuna feature corrisponde
+		 * al modello costruito con la combinazione di parametri
+		 * che minimizza il valore del RMSE
+		 */
 		setModels(opt.computeOptimalVARModel(resultRToJava, schema));
 	}
+
+	/*
+	 * costruttore della classe
+	 * setta l'attributo models 
+	 * con l'arrayList che arriva in input
+	 * tale costruttore viene utilizzato per costruire un oggetto
+	 * forecastingmodel dove i modelli VAR per ciascuna feature 
+	 * sono costruiti tutti conl la stessa combinazione di parametri
+	 */
+	public ForecastingModel(ArrayList<FeatureForecastingModel> mod) {
+		setModels(mod);
+	}
+
+	
+	public String toString() {
+		String str = "MODELLO VAR:" + "\n";
+		;
+		for (FeatureForecastingModel VARModel : models) {
+			str += VARModel.toString() + "\n";
+		}
+
+		return str;
+	}
+
+
+
+	/*
+	 * restituisca un iteratore 
+	 */
+	public Iterator<FeatureForecastingModel> iterator() {
+
+		return this.models.iterator();
+	}
+
+	
+	/*
+	 * restituisca il modello VAR per la specifica feature
+	 * che viene passata in input
+	 */
+	public FeatureForecastingModel getFeatureForecastingModel(Feature f) {
+		return models.get(f.getFeatureIndex());
+	}
+	
+	
+	/*
+	 * metodo privato
+	 * setta l'attributo models
+	 */
+	private void setModels(ArrayList<FeatureForecastingModel> mod) {
+		this.models = mod;
+	}
+	
 
 	private Double[][] doubleToDouble(double[][] conv) {
 		int rows = conv.length;
@@ -41,49 +132,5 @@ public class ForecastingModel implements Iterable<FeatureForecastingModel>,
 		return res;
 
 	}
-
-	public ForecastingModel(ArrayList<FeatureForecastingModel> mod) {
-		setModels(mod);
-	}
-
-	public String toString() {
-		String str = "MODELLO VAR:" + "\n";
-		;
-		for (FeatureForecastingModel VARModel : models) {
-			str += VARModel.toString() + "\n";
-		}
-
-		return str;
-	}
-
-	/*
-	 * private void initializesModels(ArrayList<Object> resultExecutR,
-	 * SnapshotSchema schema) { models = new
-	 * ArrayList<FeatureForecastingModel>(schema.getTargetList() .size());
-	 * ForecastingModelIndex index = new ForecastingModelIndex();
-	 * ArrayList<ArrayList<Feature>> listOfCorrelatedFeature =
-	 * (ArrayList<ArrayList<Feature>>) resultExecutR .get(index.feature);
-	 * ArrayList<ArrayList<ArrayList<Double>>> listOfCorrelatedCoefficients =
-	 * (ArrayList<ArrayList<ArrayList<Double>>>) resultExecutR
-	 * .get(index.coefficients);
-	 * 
-	 * for (Feature f : schema.getTargetList()) { int i = f.getIndexMining() -
-	 * schema.getSpatialList().size(); FeatureForecastingModel VARModel = new
-	 * FeatureVARForecastingModel( f, listOfCorrelatedFeature.get(i),
-	 * listOfCorrelatedCoefficients.get(i)); models.add(i, VARModel); } }
-	 */
-
-	@Override
-	public Iterator<FeatureForecastingModel> iterator() {
-
-		return this.models.iterator();
-	}
-
-	private void setModels(ArrayList<FeatureForecastingModel> mod) {
-		this.models = mod;
-	}
-
-	public FeatureForecastingModel getFeatureForecastingModel(Feature f) {
-		return models.get(f.getFeatureIndex());
-	}
+	
 }
